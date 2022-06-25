a40216fdc16848cdad530eefe693611b1c2fcf63
Lucas Braz <lucasbrzdias@gmail.com>
2022-06-24 23:54:44 -0300

    feat: 🎸 update

--- wb_frontend/src/pages/cadastro.tsx
+++ wb_frontend/src/pages/cadastro.tsx
@@ -1,10 +1,68 @@
 import 'materialize-css/dist/css/materialize.min.css'
 import '../styles/cadastro.css'
 import  Navbar  from '../components/nav';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup/dist/yup';
+import { useCallback } from 'react';
+import { api } from '../services/api';
 
+interface CadastroProps {
+    nome: string;
+    sobreNome: string;
+    estado: string;
+    cidade: string;
+    bairro: string;
+    rua: string;
+    numero: number;
+    codigoPostal: string;
+    informacoesAdicionais: string;
+}
 
 
 function CadastroClientes (){
+    const {
+        register,
+        handleSubmit,
+      } = useForm<CadastroProps>({
+        mode: 'onBlur',
+      });
+
+      const onSubmit = useCallback(
+        async ({
+            nome, 
+            sobreNome, 
+            estado, 
+            cidade, 
+            bairro, 
+            rua, 
+            numero, 
+            codigoPostal, 
+            informacoesAdicionais
+        }: CadastroProps) => {
+
+          await api.post('/cliente/cadastrar', {
+            nome,
+            sobreNome,
+            endereco: {
+                estado,
+                cidade,
+                bairro,
+                rua,
+                numero,
+                codigoPostal,
+                informacoesAdicionais
+            }
+            }).then(response => {
+                if (response.status === 200) alert('Cliente cadastrado com sucesso!');
+                // if (status === 200) alert('Cliente cadastrado com sucesso!');
+            }).catch(error => {
+                console.log(error);
+                alert('Erro ao cadastrar')
+            })
+        },
+        [],
+      );
+
         return (
             <> 
                 <Navbar/>
@@ -12,37 +70,34 @@ function CadastroClientes (){
                         <div className="row box">
                                 <h4 className='subtitulo margin_left'>Cadastro de Cliente</h4>
                                 <div className="row">
-                                    <form className="col s12">
+                                    <form onSubmit={handleSubmit(onSubmit)} className="col s12">
                                         <div className="row">
                                             <div className="input-field col s6">
-                                                <input id="icon_prefix " type="text" className="validate" placeholder="Nome"/>                                           
+                                                <input id="icon_prefix " type="text" className="validate" placeholder="Nome" {...register('nome')}/>                                           
                                             </div>
                                             <div className="input-field col s6">
-                                                <input id="icon_prefix " type="text" className="validate" placeholder= "Sobrenome"/>
+                                                <input id="icon_prefix " type="text" className="validate" placeholder= "Sobrenome" {...register('sobreNome')} />
                                             </div>
                                             <div className="input-field col s6">
-                                                <label>Endereço</label>
+                                                <input type="text" className="validate" placeholder="Estado" {...register('estado')} />
                                             </div>
                                             <div className="input-field col s6">
-                                                <input id="telefone" type="text" className="validate" placeholder= "Estado"/>
+                                                <input type="text" className="validate" placeholder="Cidade" {...register('cidade')} />
                                             </div>
                                             <div className="input-field col s6">
-                                                <input type="text" className="validate" placeholder= "Cidade"/>
+                                                <input type="text" className="validate" placeholder="Bairro" {...register('bairro')} />
                                             </div>
                                             <div className="input-field col s6">
-                                                <input type="text" className="validate" placeholder= "Bairro"/>
+                                                <input type="text" className="validate" placeholder="Rua" {...register('rua')}  />
                                             </div>
                                             <div className="input-field col s6">
-                                                <input type="text" className="validate" placeholder= "Rua"/>
+                                                <input type="number" className="validate" placeholder="Número" {...register('numero')}  />
                                             </div>
                                             <div className="input-field col s6">
-                                                <input type="text" className="validate" placeholder= "Número"/>
+                                                <input type="text" className="validate" placeholder="Código Postal" {...register('codigoPostal')} />
                                             </div>
                                             <div className="input-field col s6">
-                                                <input type="text" className="validate" placeholder= "Código Postal"/>
-                                            </div>
-                                            <div className="input-field col s6">
-                                                <input type="text" className="validate" placeholder= "Informações Adicionais" />
+                                                <input type="text" className="validate" placeholder="Informações Adicionais" {...register('informacoesAdicionais')} />
                                             </div>
                                             <div className="row">
                                                 <div className="col s12">
--- wb_frontend/src/pages/user.tsx
+++ wb_frontend/src/pages/user.tsx
@@ -1,71 +1,119 @@
 import 'materialize-css/dist/css/materialize.min.css'
 import '../styles/cadastro.css'
 import  Navbar  from '../components/nav';
-import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { useCallback } from 'react';
+import { api } from '../services/api';
+import { useParams } from 'react-router-dom';
+
+interface CadastroProps {
+    nome: string;
+    sobreNome: string;
+    estado: string;
+    cidade: string;
+    bairro: string;
+    rua: string;
+    numero: number;
+    codigoPostal: string;
+    informacoesAdicionais: string;
+}
 
 
 function User (){
-    return (
-        <> 
-            <Navbar/>
+    const {
+        register,
+        handleSubmit,
+      } = useForm<CadastroProps>({
+        mode: 'onBlur',
+      });
+      const { id } = useParams();
 
-            <div className='topo'>
+      const onSubmit = useCallback(
+        async ({
+            nome, 
+            sobreNome, 
+            estado, 
+            cidade, 
+            bairro, 
+            rua, 
+            numero, 
+            codigoPostal, 
+            informacoesAdicionais
+        }: CadastroProps) => {
+          await api.put(`cliente/atualizar/${id}`, {
+            nome,
+            sobreNome,
+            endereco: {
+                estado,
+                cidade,
+                bairro,
+                rua,
+                numero,
+                codigoPostal,
+                informacoesAdicionais
+            }
+            }).then(response => {
+                if (response.status === 200) alert('Cliente alterado com sucesso!');
+                // if (status === 200) alert('Cliente cadastrado com sucesso!');
+            }).catch(error => {
+                console.log(error);
+                alert('Erro ao cadastrar')
+            })
+        },
+        [],
+      );
+
+        return (
+            <> 
+                <Navbar/>
+                <div className='topo'>
                         <div className="row box">
-                                <h4 className='subtitulo'>Cliente</h4>
+                                <h4 className='subtitulo margin_left'>Cadastro de Cliente</h4>
                                 <div className="row">
-                                    <form className="col s12">
+                                    <form onSubmit={handleSubmit(onSubmit)} className="col s12">
                                         <div className="row">
                                             <div className="input-field col s6">
-                                                <input id="icon_prefix " type="text" className="validate" placeholder="Nome"/>                                           
+                                                <input id="icon_prefix " type="text" className="validate" placeholder="Nome" {...register('nome')}/>                                           
                                             </div>
                                             <div className="input-field col s6">
-                                                <input id="icon_prefix " type="text" className="validate" placeholder= "Sobrenome"/>
+                                                <input id="icon_prefix " type="text" className="validate" placeholder= "Sobrenome" {...register('sobreNome')} />
                                             </div>
                                             <div className="input-field col s6">
-                                                <label>Endereço</label>
+                                                <input type="text" className="validate" placeholder="Estado" {...register('estado')} />
                                             </div>
                                             <div className="input-field col s6">
-                                                <input id="telefone" type="text" className="validate" placeholder= "Estado"/>
+                                                <input type="text" className="validate" placeholder="Cidade" {...register('cidade')} />
                                             </div>
                                             <div className="input-field col s6">
-                                                <input type="text" className="validate" placeholder= "Cidade"/>
+                                                <input type="text" className="validate" placeholder="Bairro" {...register('bairro')} />
                                             </div>
                                             <div className="input-field col s6">
-                                                <input type="text" className="validate" placeholder= "Bairro"/>
+                                                <input type="text" className="validate" placeholder="Rua" {...register('rua')}  />
                                             </div>
                                             <div className="input-field col s6">
-                                                <input type="text" className="validate" placeholder= "Rua"/>
+                                                <input type="number" className="validate" placeholder="Número" {...register('numero')}  />
                                             </div>
                                             <div className="input-field col s6">
-                                                <input type="text" className="validate" placeholder= "Número"/>
+                                                <input type="text" className="validate" placeholder="Código Postal" {...register('codigoPostal')} />
                                             </div>
                                             <div className="input-field col s6">
-                                                <input type="text" className="validate" placeholder= "Código Postal"/>
+                                                <input type="text" className="validate" placeholder="Informações Adicionais" {...register('informacoesAdicionais')} />
                                             </div>
-                                            <div className="input-field col s6">
-                                                <input type="text" className="validate" placeholder= "Informações Adicionais" />
-                                            </div>
-                                            <div className='input-field col s6 botoes_user margin_left'>
-                                                <div className="row">
-                                                    <div className="col s12">
-                                                        <button className='btn waves-effect waves-light botao' type="submit" name="action">Salvar
-                                                        </button>
-                                                    </div>
-                                                </div>
-                                                <div className="row">
-                                                    <div className="col s12">
-                                                        <button className='btn waves-effect waves-light botao_cancelar' type="submit" name="action">Cancelar
-                                                        </button>
-                                                    </div>
+                                            <div className="row">
+                                                <div className="col s12">
+                                                    <button className='btn waves-effect waves-light botao' type="submit" name="action">Cadastrar
+                                                    </button>
                                                 </div>
                                             </div>
                                         </div>
                                     </form>
                                 </div>                       
                         </div>
+                        <br />
                 </div>
-        </>
-    )
-}
+            </>
+        )
+    }
+
 
 export default User;
